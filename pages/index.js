da7f0b5bd568fba6cf7ada605781485220f36cea
daskiro514 <luckybuzstar@gmail.com>
2022-09-21 22:59:04 +0900

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,66 @@
+import Head from 'next/head'
+
+import { getEvents, getPosts } from '../utils/wordpress'
+
+import Post from '../components/Post'
+import Event from '../components/Event'
+
+export async function getStaticProps({ params }) {
+  const posts = await getPosts()
+  const events = await getEvents()
+
+  return {
+    props: {
+      posts,
+      events,
+    },
+    revalidate: 10, // In seconds
+  }
+}
+
+export default function Home({ posts, events }) {
+  console.log(events)
+
+  // return (
+  //   <div>Home</div>
+  // )
+
+  const jsxPosts = posts.map((post) => {
+    const featuredMedia = post['_embedded']['wp:term'][0]
+    return <Post post={post} featuredMedia={featuredMedia} key={post.id} />
+  })
+
+  const jsxEvents = events.map((event) => {
+    // const featuredMedia = event['_embedded']['wp:term'][0]
+    return <Event event={event} key={event.id} />
+    // return <Event event={event} featuredMedia={featuredMedia} key={event.id} />
+  })
+
+  return (
+    <>
+      <Head>
+        <title>Tech Blog</title>
+        <meta
+          name="description"
+          content="Keep up to date with the latest trends in tech"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        {/* You can add more metadata here, like open graph tags for social media, etc */}
+      </Head>
+
+      <div className="container pt-5">
+        <h1 className="text-center pb-5">Tech Blog</h1>
+        <div className="row">
+          <div className="col-lg-8">
+            <h2 className="pb-3">Our blog posts</h2>
+            {jsxPosts}
+          </div>
+          <div className="col-lg-4">
+            <h2 className="pb-3">Events</h2>
+            {jsxEvents}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
